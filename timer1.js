517e6a12eeb594ff0df888779b6a9785876a87b1
marcellmueller <mlmueller@protonmail.com>
2020-11-02 20:26:41 -0800

    completed timer1.js

---
+++ timer1.js
@@ -0,0 +1,31 @@
+const args = process.argv.slice(2);
+
+const removeNonNumbers = () => {
+  let newArray = [];
+  //convert args to integers using .map
+  const arr = args.map(Number);
+
+  //loop through new array to filter non numbers
+  for (const each of arr) {
+    if (Number.isInteger(each) && each > 0) {
+      newArray.push(each);
+    }
+  }
+  return newArray;
+};
+
+//call for new array filtered to have only numbers
+const seconds = removeNonNumbers(args);
+const alarms = removeNonNumbers(args);
+
+const setAlarms = (alarms) => {
+  for (let i = 0; i < alarms.length; i++) {
+    setTimeout(() => {
+      //process.stdout.write('\x07'); wasn't beeping
+      process.stdout.write("*");
+      console.log(`Alarm ${i}: ${seconds[i]} seconds`);
+    }, alarms[i] * 1000);
+  }
+};
+
+setAlarms(alarms);
